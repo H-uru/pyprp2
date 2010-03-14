8a3ec09516a9169b486a58ba70227b640f5df217
lontahv <lontahv@gmail.com>
2010-03-14 22:05:47 +0000

    Added framework for plasma modifiers.

--- modules/plasma/plasma_modifiers_dialog.py
+++ modules/plasma/plasma_modifiers_dialog.py
@@ -1,5 +1,53 @@
-import bpy
+from plasma_namespace import *
 from bpy.props import *
+
+def AddModifier(modifiers,typestr):
+    #if it's already there create a new name
+    newmod = modifiers.add()
+    if modifiers.get(typestr) == None:
+        newmod.name = typestr
+    else:
+        trynum = 1
+        while modifiers.get(typestr+str(trynum)):
+            trynum+=1
+        newmod.name = typestr+str(trynum)
+        
+    newmod.StringProperty(attr="type", default=typestr)
+    print("Adding %s"%newmod.name)
+    if typestr == "plSpawnModifier":
+        newmod.FloatProperty(attr="test")
+
+class PlCreateSpawnModifier(bpy.types.Operator):
+    bl_idname = "object.plcreatespawnmodifier"
+    bl_label = "Create a Spawn Mod"
+    def poll(self, context):
+        return context.active_object != None
+    def execute(self, context):
+        AddModifier(context.object.plasma_settings.modifiers,"plSpawnModifier")
+        return {'FINISHED'}
+
+class PlCreateWavesetModifier(bpy.types.Operator):
+    bl_idname = "object.plcreatewavesetmodifier"
+    bl_label = "Create a Waveset Mod"
+    def poll(self, context):
+        return context.active_object != None
+    def execute(self, context):
+        AddModifier(context.object.plasma_settings.modifiers,"plWavesetModifier")
+        return {'FINISHED'}
+
+class PlAddModifierMenu(bpy.types.Menu):
+    bl_idname = "PlAddModifierMenu"
+    bl_label = "Add Plasma Modifier"
+
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.operator_context = 'EXEC_AREA'
+        layout.operator("object.plcreatespawnmodifier", text="Spawn Point")
+        layout.operator("object.plcreatewavesetmodifier", text="Waveset")
+class plModifierSettings(bpy.types.IDPropertyGroup):
+    pass
+
 
 class PlasmaModTypes(bpy.types.Menu):
     bl_idname = "PlasmaModTypes"
@@ -17,29 +65,32 @@ class plasma_modifiers(bpy.types.Panel):
     bl_region_type = 'WINDOW'
     bl_context = "modifier"
     bl_label = "Plasma Modifiers"
-    bpy.types.Object.FloatProperty(attr="testprop",name="Mass")
-    bpy.types.Object.PointerProperty(attr="custom", name="custom", type=bpy.types.IDPropertyGroup)
-    bpy.types.Object.CollectionProperty(attr="plModifiers", type=bpy.types.IDPropertyGroup)
-                                 
-    #bpy.types.Object.EnumProperty(attr="plModifiers", items=(("None", "Box",""),("test", "Sphere","")), name="plModifiers")
+    
+    bpy.types.Object.PointerProperty(attr="plasma_settings", type=PlasmaSettings, name="Plasma Settings", description="Plasma Engine Object Settings")
+    PlasmaSettings.CollectionProperty(attr="modifiers", type=plModifierSettings)
     def draw(self, context):
         layout = self.layout
 
         ob = context.object
-
-        #layout.operator_menu_enum("object.modifier_add", "testprop")
+        pl = ob.plasma_settings
+        layout.menu("PlAddModifierMenu")
         #layout.template_ID(ob, "parent")
-        layout.menu("INFO_MT_file")
-        layout.template_list(ob, "plModifiers", ob, "custom", rows=2)
-        
-        for md in ob.modifiers:
-            box = layout.template_modifier(md)
-            #if box:
-                # match enum type to our functions, avoids a lookup table.
-             #   getattr(self, md.type)(box, ob, md, wide_ui)
+        #layout.template_list(pl, "plModifiers", pl, "custom", rows=2)
+        for modkey in pl.modifiers.keys():
+            layout.label(text=modkey)
+            #box = layout.template_modifier(md)
+
+mod_creators = [PlCreateSpawnModifier, PlCreateWavesetModifier]
 
 def register():
+    for mc in mod_creators:
+        bpy.types.register(mc)
+    bpy.types.register(PlAddModifierMenu)
     bpy.types.register(plasma_modifiers)
 
+
 def unregister():
+    for mc in mod_creators:
+        bpy.types.unregister(mc)
+    bpy.types.unregister(PlAddModifierMenu)
     bpy.types.unregister(plasma_modifiers)
