79a7da551cb22ae4a83baba5a023a213fdf31cd2
lontahv <lontahv@gmail.com>
2010-04-02 22:15:00 +0000

    Icicle bounds issue fixed.

--- modules/plasma/utils.py
+++ modules/plasma/utils.py
@@ -7,9 +7,9 @@ def transform_vector3_by_blmat(vector,m):
     return [x,y,z]
 
 def transform_vector3_by_plmat(vector,m):
-    x = m[0, 0]*vector[0] + m[1, 0]*vector[1] + m[2, 0]*vector[2] + m[3, 0]
-    y = m[0, 1]*vector[0] + m[1, 1]*vector[1] + m[2, 1]*vector[2] + m[3, 1]
-    z = m[0, 2]*vector[0] + m[1, 2]*vector[1] + m[2, 2]*vector[2] + m[3, 2]
+    x = m[0, 0]*vector[0] + m[0, 1]*vector[1] + m[0, 2]*vector[2] + m[0, 3]
+    y = m[1, 0]*vector[0] + m[1, 1]*vector[1] + m[1, 2]*vector[2] + m[1, 3]
+    z = m[2, 0]*vector[0] + m[2, 1]*vector[1] + m[2, 2]*vector[2] + m[2, 3]
     return [x,y,z]
 
 def blMatrix44_2_hsMatrix44(blmat):
