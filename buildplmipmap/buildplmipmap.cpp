dde899dfe5bfb39958546792c47ba357d1e41131
lontahv <lontahv@gmail.com>
2010-04-15 01:40:12 +0000

    Added trimmings.

--- buildplmipmap/buildplmipmap.cpp
+++ buildplmipmap/buildplmipmap.cpp
@@ -1,3 +1,22 @@
+#    Copyright (C) 2010  Guild of Writers PyPRP2 Project Team
+#    See the file AUTHORS for more info about the team
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#    Please see the file LICENSE for the full license.
+
 #include "ResManager/plResManager.h"
 #include "PRP/plPageInfo.h"
 #include "PRP/KeyedObject/plLocation.h"
