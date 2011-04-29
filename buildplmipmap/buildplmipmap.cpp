ad9e21a8291393ab50a6a3301b6bb0c90d161719
lontahv <lontahv@gmail.com>
2011-04-28 23:28:34 -0600

    Updated buildplmipmap to use plMipmap compression function. Got rid of old MSVC files.

--- buildplmipmap/buildplmipmap.cpp
+++ buildplmipmap/buildplmipmap.cpp
@@ -1,10 +1,28 @@
+/* This file is part of PyPRP2.
+ *
+ * Copyright (C) 2010 PyPRP2 Project Team
+ * See the file AUTHORS for more info about the team.
+ *
+ * PyPRP2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PyPRP2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PyPRP2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <ResManager/plResManager.h>
 #include <PRP/plPageInfo.h>
 #include <PRP/KeyedObject/plLocation.h>
 #include <PRP/Surface/plMipmap.h>
 #include <Util/plString.h>
 #include <Stream/hsStream.h>
-#include "squish.h"
 
 #include <IL/il.h>
 #include <IL/ilu.h>
@@ -57,16 +75,7 @@ int main(int argc, char **argv) {
             if (level != 0)
                 iluScale(width, height, 1);
             ilCopyPixels(0, 0, 0, width, height, 1, IL_RGBA, IL_UNSIGNED_BYTE, ldata);
-
-            unsigned char* temp_compressed_data = new unsigned char[size];
-
-            if (mip.getDXCompression() == plMipmap::kDXT1)
-                squish::CompressImage(ldata, width, height, temp_compressed_data, squish::kDxt1 | squish::kColourRangeFit);
-            else if (mip.getDXCompression() == plMipmap::kDXT5)
-                squish::CompressImage(ldata, width, height, temp_compressed_data, squish::kDxt5 | squish::kColourRangeFit);
-            mip.setLevelData(level,temp_compressed_data, size);
-            delete[] temp_compressed_data;
-            delete[] ldata;
+            mip.CompressImage(level, ldata, size);
         }
         hsFileStream os;
         os.open(argv[2],fmWrite);
